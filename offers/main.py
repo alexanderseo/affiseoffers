00851608a248c0352f9900045f24d23f098020d1
alexanderseo <alexanderzanin@yandex.ru>
2020-08-07 18:09:28 +0300

    Методы для работы с API AFFISE

--- offers/main.py
+++ offers/main.py
@@ -3,17 +3,29 @@ import pandas as pd
 import json
 
 
-class GetAllOffers:
+class InfoMethodsOffers:
     base_endpoints = 'http://api.cpanomer1.affise.com/3.0/'
 
     def __init__(self, headers):
         self.headers = headers
 
     def offersListToJson(self):
-        affise_response = requests.get(GetAllOffers.base_endpoints + 'offers', headers=self.headers)
-        json_response = affise_response.json()
-        with open('alloffers.json', 'w', encoding='utf-8') as file:
-            json.dump(json_response, file, indent=2, ensure_ascii=False)
+        try:
+            affise_response = requests.get(InfoMethodsOffers.base_endpoints + 'offers', headers=self.headers)
+            affise_response.raise_for_status()
+            json_response = affise_response.json()
+            with open('alloffers.json', 'w', encoding='utf-8') as file:
+                json.dump(json_response, file, indent=2, ensure_ascii=False)
+            print('Документ с доступными Офферами создан: alloffers.json')
+        except requests.exceptions.HTTPError as errorhttp:
+            print("Http Error:", errorhttp)
+        except requests.exceptions.ConnectionError as errorconnection:
+            print("Error Connecting:", errorconnection)
+        except requests.exceptions.Timeout as errortimeout:
+            print("Timeout Error:", errortimeout)
+        except requests.exceptions.RequestException as error:
+            print("Oops: Something Else", error)
+
 
     def readOffersFile(self):
         with open("alloffers.json", encoding='utf-8') as fileoffers:
@@ -28,16 +40,38 @@ class GetAllOffers:
             print(table)
 
     def offersListByCategoriesToJson(self, categories):
-        affise_response = requests.get(GetAllOffers.base_endpoints + 'offers' + '?' 'categories' + '=' + categories, headers=self.headers)
-        json_response = affise_response.json()
-        with open('offersbycategories.json', 'w', encoding='utf-8') as file:
-            json.dump(json_response, file, indent=2, ensure_ascii=False)
+        try:
+            affise_response = requests.get(InfoMethodsOffers.base_endpoints + 'offers?countries=' + categories, headers=self.headers)
+            affise_response.raise_for_status()
+            json_response = affise_response.json()
+            with open('offersbycategories.json', 'w', encoding='utf-8') as file:
+                json.dump(json_response, file, indent=2, ensure_ascii=False)
+            print('Документ c офферами по категории создан: offersbycategories.json')
+        except requests.exceptions.HTTPError as errorhttp:
+            print("Http Error:", errorhttp)
+        except requests.exceptions.ConnectionError as errorconnection:
+            print("Error Connecting:", errorconnection)
+        except requests.exceptions.Timeout as errortimeout:
+            print("Timeout Error:", errortimeout)
+        except requests.exceptions.RequestException as error:
+            print("Oops: Something Else", error)
 
     def offerCategoriesToJson(self):
-        affise_response = requests.get(GetAllOffers.base_endpoints + 'offer/categories', headers=self.headers)
-        json_response = affise_response.json()
-        with open('offercategories.json', 'w', encoding='utf-8') as file:
-            json.dump(json_response, file, indent=2, ensure_ascii=False)
+        try:
+            affise_response = requests.get(InfoMethodsOffers.base_endpoints + 'offer/categories', headers=self.headers)
+            affise_response.raise_for_status()
+            json_response = affise_response.json()
+            with open('offercategories.json', 'w', encoding='utf-8') as file:
+                json.dump(json_response, file, indent=2, ensure_ascii=False)
+            print('Документ c доступными категориями офферов создан: offersbycategories.json')
+        except requests.exceptions.HTTPError as errorhttp:
+            print("Http Error:", errorhttp)
+        except requests.exceptions.ConnectionError as errorconnection:
+            print("Error Connecting:", errorconnection)
+        except requests.exceptions.Timeout as errortimeout:
+            print("Timeout Error:", errortimeout)
+        except requests.exceptions.RequestException as error:
+            print("Oops: Something Else", error)
 
     def tableViewForOfferCategories(self):
         with open("offercategories.json", encoding='utf-8') as file_offers:
@@ -46,8 +80,37 @@ class GetAllOffers:
             offers = pd.json_normalize(z, 'categories')
             print(offers)
 
+    def offerById(self, ID):
+        try:
+            affise_response = requests.get(InfoMethodsOffers.base_endpoints + 'offer/' + ID, headers=self.headers)
+            affise_response.raise_for_status()
+            json_response = affise_response.json()
+            with open('offerID.json', 'w', encoding='utf-8') as file:
+                json.dump(json_response, file, indent=2, ensure_ascii=False)
+            print('Документ с оффером %s: offerID.json' % ID)
+        except requests.exceptions.HTTPError as errorhttp:
+            print("Http Error:", errorhttp)
+        except requests.exceptions.ConnectionError as errorconnection:
+            print("Error Connecting:", errorconnection)
+        except requests.exceptions.Timeout as errortimeout:
+            print("Timeout Error:", errortimeout)
+        except requests.exceptions.RequestException as error:
+            print("Oops: Something Else", error)
+
+    def resultInfoOfferID(self):
+        with open("offerID.json", encoding='utf-8') as fileoffers:
+            readfileoffers = fileoffers.read()
+            z = json.loads(readfileoffers)
+            offers = pd.json_normalize(z)
+        offer_ID = pd.Series(offers['offer.id'])
+        offer_Title = pd.Series(offers['offer.title'])
+        offer_Countries = pd.Series(offers['offer.countries'])
+        return 'Оффер ID: {0} - {1}, страны в Оффере: {2}'.format(offer_ID.item(), offer_Title.item(), offer_Countries.item())
+
+
+
 
 if __name__ == "__main__":
-    headers = {'API-Key': 'e60a98867d363b0d43b9e7c58ec498ed'}
-    x = GetAllOffers(headers)
-    x.tableViewForOfferCategories()
+    headers = {'API-Key': ''}
+    x = InfoMethodsOffers(headers)
+    print(x.resultInfoOfferID())
